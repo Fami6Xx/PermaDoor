1d59a01b52b7bd82e2fd196531348c2e48725288
Fami6Xx <libojer@gmail.com>
2023-07-22 03:31:25 +0200

    feat: getting more data from discord

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import DiscordProvider from "next-auth/providers/discord"
+import {prisma} from "@/lib/prisma";
+import {PrismaAdapter} from "@auth/prisma-adapter";
 
 export const authOptions = {
+	adapter: PrismaAdapter(prisma),
 	secret: process.env.AUTH_SECRET,
 	providers: [
 		GoogleProvider({
@@ -11,9 +14,41 @@ export const authOptions = {
 		}),
 		DiscordProvider({
 			clientId: process.env.DISCORD_CLIENT_ID,
-			clientSecret: process.env.DISCORD_CLIENT_SECRET
+			clientSecret: process.env.DISCORD_CLIENT_SECRET,
+			profile(profile) {
+				if (profile.avatar === null) {
+					const defaultAvatarNumber = parseInt(profile.discriminator) % 5
+					profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
+				} else {
+					const format = profile.avatar.startsWith("a_") ? "gif" : "png"
+					profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`
+				}
+				console.log(profile);
+				return {
+					id: profile.id,
+					discordId: profile.id,
+					name: profile.username,
+					global_name: profile.global_name,
+					two_factor_enabled: profile.mfa_enabled,
+					email: profile.email,
+					image: profile.image_url,
+				}
+			},
 		})
 	],
+	callbacks: {
+		async session({ session, token, user }) {
+			// Send properties to the client, like an access_token and user id from a provider.
+			session.user.id = user.discordId;
+			session.user.name = user.name;
+			session.user.displayName = user.global_name;
+			session.user.two_factor_enabled = user.two_factor_enabled;
+			session.user.email = user.email;
+			session.user.image = user.image;
+
+			return session
+		}
+	}
 };
 
 const handler = NextAuth(authOptions)
