a1a53f4cc33631a6ac5ba4a1a62fc6062c0e8940
Fami6Xx <libojer@gmail.com>
2023-07-30 06:43:13 +0200

    feat: added theme provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body>
         <Providers>
           <PermaNav/>
--- src/app/providers.js
+++ src/app/providers.js
@@ -1,11 +1,14 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react'
+import {ThemeProvider} from "next-themes";
 
 export function Providers({children}) {
 	return (
 		<NextUIProvider>
-			{children}
+			<ThemeProvider attribute="class" enableSystem={true}>
+				{children}
+			</ThemeProvider>
 		</NextUIProvider>
 	)
 }
