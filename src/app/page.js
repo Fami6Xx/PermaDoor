a50669c34455ca4c30738d0ca4f71b5b5034748b
Fami6Xx <libojer@gmail.com>
2023-07-30 07:34:41 +0200

    refactor: removed unused session from index page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   return (
     <>
       <p>First site</p>
