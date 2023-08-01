f922fc978e7aceb3a937ea61cebc66a187d6f27e
Fami6Xx <libojer@gmail.com>
2023-08-01 04:40:02 +0200

    feat: added og

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,19 @@ import PermaNav from "@/components/Navbar/PermaNav";
 export const metadata = {
   title: 'PermaDoor',
   description: 'Definitely not a Backdoor',
+  openGraph: {
+    title: "PermaDoor",
+    description: "This site was made only for testing and learning purposes by developer Fami6Xx",
+    url: "https://perma-door.vercel.com",
+    siteName: "PermaDoor",
+    type: "website",
+    locale: "en_US",
+    images: [{
+      url: "https://cdn.discordapp.com/app-icons/1126625590770925628/ba49f2dbe1be940037b34820f0b1a23c.png?size=256",
+      width: 256,
+      height: 256
+    }]
+  }
 }
 
 export default function RootLayout({ children }) {
