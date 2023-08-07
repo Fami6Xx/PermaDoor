4b1dc071ef02630807162c842ef95dffa61fb189
Fami6Xx <libojer@gmail.com>
2023-08-07 18:03:49 +0200

    feat: added pusher

---
+++ src/lib/pusher.js
@@ -0,0 +1,17 @@
+import Pusher from 'pusher'
+import PusherClient from 'pusher-js'
+
+export const pusherServer = new Pusher({
+	appId: process.env.PUSHER_APP_ID,
+	key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+	secret: process.env.PUSHER_APP_SECRET,
+	cluster: 'eu',
+	useTLS: true,
+})
+
+export const pusherClient = new PusherClient(
+	process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+	{
+		cluster: 'eu',
+	}
+)
