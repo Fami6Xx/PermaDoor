f473eb0b02a66e7d2ee92aab98c72d80ae318cf4
Fami6Xx <libojer@gmail.com>
2023-08-07 10:54:43 +0200

    feat: improved loading time & removed unneeded request to backend

--- src/components/Navbar/user/NavbarUser.js
+++ src/components/Navbar/user/NavbarUser.js
@@ -2,17 +2,9 @@ import NavbarSimpleAuth from "@/components/Navbar/user/auth/NavbarSimpleAuth";
 import NavbarNextButton from "@/components/Navbar/user/auth/NavbarNextButton";
 import NavbarNextModal from "@/components/Navbar/user/auth/NavbarNextModal";
 import NavbarUserDrop from "@/components/Navbar/user/NavbarUserDrop";
-import {getProviders} from "next-auth/react";
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 
 const NavbarUser = async ({authentication, session}) => {
 	// ToDo: Until not fixed in react-aria this dropdown wont work in server components
-	const providers = await getProviders();
-	let styles = {};
-
-	authOptions.providers.map((config) => (
-		styles[config.id] = config.style
-	));
 
 	if(session) {
 		return (
@@ -25,7 +17,7 @@ const NavbarUser = async ({authentication, session}) => {
 					authentication.useNextAuth ?
 						<>
 							<NavbarNextButton>
-								<NavbarNextModal providers={providers} providerStyles={styles}/>
+								<NavbarNextModal/>
 							</NavbarNextButton>
 						</>
 						:
--- src/components/Navbar/user/auth/NavbarNextModal.js
+++ src/components/Navbar/user/auth/NavbarNextModal.js
@@ -1,7 +1,16 @@
 import {ModalBody, ModalContent, ModalFooter, ModalHeader} from "@nextui-org/modal";
 import NavbarAuthProvider from "@/components/Navbar/user/auth/NavbarAuthProvider";
+import {getProviders} from "next-auth/react";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 
-const NavbarNextModal = ({providers, providerStyles}) => {
+const NavbarNextModal = async () => {
+	const providers = await getProviders();
+	let styles = {};
+
+	authOptions.providers.map((config) => (
+		styles[config.id] = config.style
+	));
+
 	return (
 		<>
 			<ModalContent>
@@ -12,8 +21,8 @@ const NavbarNextModal = ({providers, providerStyles}) => {
 							<NavbarAuthProvider
 								providerId={provider.id}
 								providerName={provider.name}
-								logo={providerStyles[provider.id].logo}
-								logoDark={providerStyles[provider.id].logoDark}
+								logo={styles[provider.id].logo}
+								logoDark={styles[provider.id].logoDark}
 							>
 								Sign in with <b>{provider.name}</b>
 							</NavbarAuthProvider>
