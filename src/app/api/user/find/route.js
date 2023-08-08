fcde4d57b226a741cc60628c86ea8c3bc0c5de72
Fami6Xx <libojer@gmail.com>
2023-08-08 10:53:49 +0200

    feat: search user route

--- src/app/api/user/find/route.js
+++ src/app/api/user/find/route.js
@@ -1,3 +1,10 @@
+import {findUsers} from "@/lib/friends";
+import {NextResponse} from "next/server";
+
 export async function GET(request){
-	// Todo
+	const {searchParams} = new URL(request.url);
+	const query = searchParams.get("query");
+	const currentUserId = searchParams.get("currentUserId");
+
+	return NextResponse.json(await findUsers(query, currentUserId));
 }
