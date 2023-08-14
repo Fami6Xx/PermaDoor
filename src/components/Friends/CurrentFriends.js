2aabd268e8af32cee927fb639128a11839f5f325
Fami6Xx <libojer@gmail.com>
2023-08-14 21:35:36 +0200

    fix: card prototype shit mad

--- src/components/Friends/CurrentFriends.js
+++ src/components/Friends/CurrentFriends.js
@@ -8,17 +8,19 @@ const CurrentFriends = async () => {
 	const session = await getServerSession(authOptions);
 	const friends = session ? await getUserFriendsInfoById(session.user.id) : null;
 
+	console.log(friends);
+
 	if(friends && friends.length > 0) {
 		return (
-			<div className="ml-4">
+			<div className="ml-4" key="div-wrapper">
 				{friends.map((friend) => (
-					<Card shadow="sm" radius="md" key={friend.id}>
+					<Card shadow="sm" radius="md">
 						<CardBody className="flex flex-row gap-2">
-							<Image src={friend.image} alt={friend.global_name} className="rounded-full w-10 h-10"/>
+							<Image key={friend.id + "--image"} src={friend.image} alt={friend.global_name} width={64} height={64} className="rounded-full w-10 h-10"/>
 							<div className="flex flex-col">
 								<span className="text-sm font-semibold">{friend.global_name}</span>
 								<span
-									className="text-xs dark:text-gray-400 light:text-gray-500">Added on {friend.friendshipCreatedAt}</span>
+									className="text-xs dark:text-gray-400 light:text-gray-500">Added on {friend.friendshipCreatedAt.toString()}</span>
 							</div>
 						</CardBody>
 					</Card>
