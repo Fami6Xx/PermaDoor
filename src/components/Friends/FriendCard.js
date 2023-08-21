5cb34c38a982c3be1d08e5035e81344630c9456e
Fami6Xx <libojer@gmail.com>
2023-08-21 20:50:56 +0200

    feat: first try of friend modal

--- src/components/Friends/FriendCard.js
+++ src/components/Friends/FriendCard.js
@@ -4,7 +4,8 @@ import {Card, CardBody} from "@nextui-org/card";
 import Image from "next/image";
 import {timeAgo} from "@/lib/time";
 import {useDisclosure} from "@nextui-org/react";
-import {Modal, ModalContent} from "@nextui-org/modal";
+import {Modal, ModalBody, ModalContent, ModalHeader} from "@nextui-org/modal";
+import {Avatar} from "@nextui-org/avatar";
 
 const FriendCard = ({friend}) => {
 	const {isOpen, onOpen, onOpenChange} = useDisclosure();
@@ -26,11 +27,34 @@ const FriendCard = ({friend}) => {
 			</Card>
 
 			<Modal
+				isOpen={isOpen}
+				onOpenChange={onOpenChange}
+				backdrop="opaque"
+				size="xs"
+				classNames={{
+					body: "bg-gradient-to-br from-white to-default-200 dark:from-default-50 dark:to-black",
+				}}
 			>
 				<ModalContent>
 					{(onClose) => (
 						<>
-
+							<ModalHeader className="flex flex-col gap-1">
+								<Avatar
+									showFallback
+									isBordered
+									src={friend.image}
+									radius="sm"
+									aria-label="User menu"
+								/>
+								<div className="h-full text-lg font-semibold items-center flex overflow-hidden w-full">
+									<span className="w-full overflow-ellipsis whitespace-nowrap overflow-hidden">
+										{friend.global_name}
+									</span>
+								</div>
+							</ModalHeader>
+							<ModalBody>
+								Information
+							</ModalBody>
 						</>
 					)}
 				</ModalContent>
