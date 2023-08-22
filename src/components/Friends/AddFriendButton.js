bd0c19311b0adbb62f2d50dd88c09fc0562c9683
Fami6Xx <libojer@gmail.com>
2023-08-22 23:36:34 +0200

    refactor: unneeded jsx

--- src/components/Friends/AddFriendButton.js
+++ src/components/Friends/AddFriendButton.js
@@ -6,7 +6,7 @@ const AddFriendButton = () => {
 	const router = useRouter();
 
 	return (
-		<Button isIconOnly color="error" variant="faded" aria-label="Add friend" className="fixed bottom-8 right-8" onPress={() => router.push("/friends/add")}>
+		<Button isIconOnly variant="faded" aria-label="Add friend" className="fixed bottom-8 right-8" onPress={() => router.push("/friends/add")}>
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 				<path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 			</svg>
