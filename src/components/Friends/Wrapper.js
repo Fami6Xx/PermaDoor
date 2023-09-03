0b904de33c987bfe992220d75c1a52ede5af6de6
Fami6Xx <libojer@gmail.com>
2023-09-03 19:54:14 +0200

    feat: added padding

--- src/components/Friends/Wrapper.js
+++ src/components/Friends/Wrapper.js
@@ -2,7 +2,7 @@ import {ScrollShadow} from "@nextui-org/scroll-shadow";
 
 const Wrapper = ({children}) => {
 	return (
-		<ScrollShadow orientation="horizontal" hideScrollBar className="flex flex-row gap-4 max-w-[80%] md:max-w-[90%] h-full lg:max-w-[1080px]" key="wrapper">
+		<ScrollShadow orientation="horizontal" hideScrollBar className="flex flex-row gap-4 p-4 max-w-[80%] md:max-w-[90%] h-full lg:max-w-[1080px]" key="wrapper">
 			{children}
 		</ScrollShadow>
 	);
