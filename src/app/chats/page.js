bdbd1540dd4afcc6aa431089dade4f27b0443b13
Fami6Xx <libojer@gmail.com>
2023-09-29 19:21:40 +0200

    feat: implemented create chat form

--- src/app/chats/page.js
+++ src/app/chats/page.js
@@ -2,14 +2,16 @@ import {getAllConversations} from "@/lib/converstation";
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 import CreateConversationButton from "@/components/Chats/CreateConverstationButton";
+import {getUserFriendsInfoById} from "@/lib/friends";
 
 const Page = async () => {
 	const session = await getServerSession(authOptions);
+	const friends = await getUserFriendsInfoById(session.user.id);
 	const conversations = await getAllConversations(session.user.id);
 	return (
 		<>
 
-			<CreateConversationButton/>
+			<CreateConversationButton users={friends}/>
 		</>
 	);
 };
