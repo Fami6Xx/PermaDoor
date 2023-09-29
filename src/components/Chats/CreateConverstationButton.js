b6c57232c01b7342ff5419d2b31fd20a51143406
Fami6Xx <libojer@gmail.com>
2023-09-29 19:20:59 +0200

    feat: functioning CreateConversationForm.js

--- src/components/Chats/CreateConverstationButton.js
+++ src/components/Chats/CreateConverstationButton.js
@@ -2,18 +2,23 @@
 import {Button} from "@nextui-org/button";
 import {useRouter} from "next/navigation";
 import {Tooltip} from "@nextui-org/tooltip";
+import {Popover, PopoverTrigger, PopoverContent} from "@nextui-org/popover";
+import CreateConversationForm from "@/components/Chats/Form/CreateConversationForm";
 
-const CreateConversationButton = () => {
-	const router = useRouter();
-
+const CreateConversationButton = ({users}) => {
 	return (
-		<Tooltip showArrow="true" content="Create a new conversation" placement="left">
-			<Button isIconOnly variant="ghost" aria-label="Add friend" className="fixed bottom-8 right-8" onPress={() => router.push("/chats/create")}>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-					<path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-				</svg>
-			</Button>
-		</Tooltip>
+		<Popover showArrow="true">
+			<PopoverTrigger>
+				<Button isIconOnly variant="ghost" aria-label="Add friend" className="fixed bottom-8 right-8">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+						<path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+					</svg>
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent>
+				<CreateConversationForm users={users}/>
+			</PopoverContent>
+		</Popover>
 	);
 };
 
--- src/components/Chats/Form/CreateConversationForm.js
+++ src/components/Chats/Form/CreateConversationForm.js
@@ -3,23 +3,44 @@
 import SelectUsers from "@/components/Chats/Form/SelectUsers";
 import {Button} from "@nextui-org/button";
 import {Input} from "@nextui-org/input";
-import ConversationKeyInput from "@/components/Chats/Form/ConversationKeyInput";
-import {Switch} from "@nextui-org/switch";
+import {useState} from "react";
+import {useSession} from "next-auth/react";
 
 const CreateConversationForm = ({users}) => {
+	const session = useSession();
+	const [value, setValue] = useState("");
+	const [selectedUsers, setSelectedUsers] = useState([]);
+
+	const selectValueChanged = (value) => {
+		setSelectedUsers(Array.from(value.values()));
+	}
+
+	const sendForm = async () => {
+		const response = await fetch("/api/chats/create", {
+			method: "POST",
+			body: JSON.stringify({
+				userId: session.data.user.id,
+				name: value,
+				users: selectedUsers
+			})
+		}).then((res) => res.json()).catch((err) => console.error(err));
+	}
+
 	return (
-		<div className="flex flex-col gap-6 pt-8">
-			<div>
-				<Input type="text" variant="bordered" label="Name of conversation" labelPlacement="outside"/>
-			</div>
-			<div className="flex flex-col gap-4 w-full">
-				<ConversationKeyInput/>
-				<Switch defaultSelected color="default" className="w-full" size="sm">Save key in browser</Switch>
-			</div>
+		<div className="flex flex-col gap-6 pt-4 pb-3">
+			<Input
+				type="text"
+				variant="bordered"
+				label="Name of conversation"
+				labelPlacement="outside"
+				placeholder="Our super secret conversation"
+				value={value}
+				onValueChange={setValue}
+			/>
 
-			<SelectUsers users={users}/>
+			<SelectUsers users={users} selectionChanged={selectValueChanged}/>
 
-			<Button>
+			<Button variant="ghost" onPress={sendForm}>
 				Create
 			</Button>
 		</div>
